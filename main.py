ab8f023931d3531e0abff3ab2be35da45d7ab6f2
tony-cannibal <castrovital.luis@gmail.com>
2023-01-16 16:31:25 -0600

    Small Update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ c_list = pd.read_excel('14100.xlsx')
 
 def clean_checklist(df):
     drop_columns = [1, 11, 13, 17, 20, 22, 23, 24] 
-    rename_columns = {'Unnamed: 11': 'Diagrama','Unnamed: 21': 'N/P', 'Unnamed: 22': 'LOT', 
+    rename_columns = {'Unnamed: 11': 'Diagrama','Unnamed: 21': 'NP', 'Unnamed: 22': 'Lot', 
                         'Unnamed: 23': 'Cantidad', 'Unnamed: 28': 'Modelo' }
     nan_value = float("NaN")
     for i in ['O', 'T', '']:
@@ -14,7 +14,9 @@ def clean_checklist(df):
     df.dropna(how="all", axis=1, inplace=True)
     df = c_list.drop(c_list.columns[drop_columns], axis=1)
     df.rename(columns= rename_columns, inplace=True)
-    df = df.reindex(columns = [col for col in df.columns if col != 'Machine'] + ['Machine'])
+    df['Unique'] = df['Nº de circuito'] + ' ' + df['NP']
+    df.drop_duplicates(subset ="Unique",keep = 'first', inplace = True)
+    df = df.reindex(columns = ['Unique'] + [col for col in df.columns if col != 'Machine' and col != 'Unique'] + ['Machine'])
     return df
 
 
